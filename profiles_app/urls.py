8010dec06abbf3ff662189dae3621de1050be6fb
Rishu <rishu@dkraftlearning.com>
2021-11-17 02:41:24 +0530

    added viewset to rest-api

--- profiles_app/urls.py
+++ profiles_app/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path,include
 
 from profiles_app import views
 
 
+
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
