c9ffe7783f68f786aef11559efe4dc019a150cb7
Rishu <rishu@dkraftlearning.com>
2021-11-17 01:17:49 +0530

    serializers

--- profiles_app/urls.py
+++ profiles_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from profiles_app import views
 
+
 urlpatterns = [
-   path('hello-view/',views.HelloApiView.as_view()),
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
--- profiles_app/views.py
+++ profiles_app/views.py
@@ -2,11 +2,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
+from rest_framework import status
+
+from profiles_app import serializers
+
+
+
+
 class HelloApiView(APIView):
     """Test API View"""
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
+        serializer_class = serializers.HelloSerializer
 
         an_apiview = [
             'Uses HTTP methods as functions (get, post, patch, put, delete)',
@@ -16,4 +24,16 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
-# Create your views here.
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
