b7d31fc19b684b1be78d6aa131b9d5fefe6c3bde
Rishu <rishu@dkraftlearning.com>
2021-11-17 12:46:16 +0530

    added filters

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -5,6 +5,7 @@ from profiles_app import models
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from profiles_app import permissions
+from rest_framework import filters
 
 from profiles_app import serializers
 
@@ -116,3 +117,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
